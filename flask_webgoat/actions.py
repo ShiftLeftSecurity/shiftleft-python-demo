a6006a4e082df1ce9b059019ad1450be748694b8
Omer Rosenblit <omer@areionsec.com>
2021-06-21 15:06:40 +0300

    fix

--- flask_webgoat/actions.py
+++ flask_webgoat/actions.py
@@ -39,12 +39,6 @@ def log_entry():
 
 @bp.route("/grep_processes")
 def grep_processes():
-    name = request.args.get("name")
-    # vulnerability: Remote Code Execution
-    res = subprocess.run(
-        ["ps aux | grep " + name + " | awk '{print $11}'"],
-        shell=True,
-        capture_output=True,
     )
     if res.stdout is None:
         return jsonify({"error": "no stdout returned"})
